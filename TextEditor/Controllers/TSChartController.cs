007db9c2c166687d1296d34c6f9f2715773d85be
ruban <JY-RedWood@JY-RUBY>
2024-06-26 14:47:39 +0800

    Add project files.

---
+++ TextEditor/Controllers/TSChartController.cs
@@ -0,0 +1,13 @@
+﻿using TextEditor.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TextEditor.Controllers
+{
+    public class TSChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TextEditor/Controllers/TextEditorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TextEditor.Controllers
+{
+    public class TextEditorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
