37baf7cc8f1134c43578f93be317c7051b8ada32
ruban <JY-RedWood@JY-RUBY>
2024-06-26 17:44:16 +0800

    Added Controller and View for Documents Text Editor pag. Added Text Editor plugin into Viiew

---
+++ TextEditor/Controllers/DocumentsController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TextEditor.Data;
+using TextEditor.Models;
+
+namespace TextEditor.Controllers
+{
+
+    // Putting [Authorize] only allows autheticated users to view this page
+    [Authorize]
+    public class DocumentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DocumentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Documents
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Docs.Include(d => d.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Documents/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Documents/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Content,UserId")] Document document)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(document);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(document);
+        }
+
+        // GET: Documents/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var document = await _context.Docs.FindAsync(id);
+            if (document == null)
+            {
+                return NotFound();
+            }
+
+            // This to allow only the user created the document to perform edit action
+            if (document.UserId != User.FindFirstValue(ClaimTypes.NameIdentifier) )
+            {
+                return NotFound();
+            }
+
+            return View(document);
+        }
+
+        // POST: Documents/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Content,UserId")] Document document)
+        {
+            if (id != document.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(document);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DocumentExists(document.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", document.UserId);
+            return View(document);
+        }
+
+        // GET: Documents/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var document = await _context.Docs
+                .Include(d => d.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (document == null)
+            {
+                return NotFound();
+            }
+            
+            // This to allow only the user created the document to perform delete action
+            if (document.UserId != User.FindFirstValue(ClaimTypes.NameIdentifier))
+            {
+                return NotFound();
+            }
+            
+            return View(document);
+        }
+
+        // POST: Documents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var document = await _context.Docs.FindAsync(id);
+            if (document != null)
+            {
+                _context.Docs.Remove(document);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DocumentExists(int id)
+        {
+            return _context.Docs.Any(e => e.Id == id);
+        }
+    }
+}
